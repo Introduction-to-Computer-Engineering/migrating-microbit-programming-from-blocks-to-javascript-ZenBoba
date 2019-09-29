58be784511b42954122b8a8944e3aa1c9b233292
ZenBoba <54600194+ZenBoba@users.noreply.github.com>
2019-09-29 00:32:29 -0600

    v0.2.js
    Input

---
+++ v0.2.js
@@ -0,0 +1,35 @@
+// A modified version of activity 1 to include input
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+# . . . #
+. # . # .
+. . # . .
+. # . # .
+# . . . #
+`)
+    basic.pause(500)
+    basic.showLeds(`
+. . # . .
+. . # . .
+# # # # # 
+. . # . .
+. . # . . 
+`)
+    basic.pause(500)
+    basic.showLeds(`
+# . . . #
+. # . # .
+. . # . .
+. # . # .
+# . . . #
+`)
+    basic.pause(500)
+    basic.showLeds(`
+. . # . .
+. . # . .
+# # # # # 
+. . # . .
+. . # . . 
+`)
+    basic.pause(500)
+})
