ccac4e61a0d346c8f1b787c5a794ea6bfff02d49
ZenBoba <54600194+ZenBoba@users.noreply.github.com>
2019-09-29 00:39:01 -0600

    v0.5.js
    Iterations

---
+++ v0.5.js
@@ -0,0 +1,36 @@
+//Spiraling sprite, press A to activate
+input.onButtonPressed(Button.A, function () {
+    for (let i = 0; i < 3; i++) {
+        sprite.move(1)
+        basic.pause(500)
+        sprite.move(1)
+        basic.pause(500)
+        sprite.move(1)
+        basic.pause(500)
+        sprite.move(1)
+        basic.pause(500)
+        sprite.turn(Direction.Right, 90)
+    }
+    for (let i = 0; i < 2; i++) {
+        sprite.move(1)
+        basic.pause(500)
+        sprite.move(1)
+        basic.pause(500)
+        sprite.move(1)
+        basic.pause(500)
+        sprite.turn(Direction.Right, 90)
+    }
+    for (let i = 0; i < 2; i++) {
+        sprite.move(1)
+        basic.pause(500)
+        sprite.move(1)
+        basic.pause(500)
+        sprite.turn(Direction.Right, 90)
+    }
+    sprite.move(1)
+    basic.pause(500)
+    sprite.turn(Direction.Right, 90)
+    sprite.move(1)
+})
+let sprite: game.LedSprite = null
+sprite = game.createSprite(0, 0)
